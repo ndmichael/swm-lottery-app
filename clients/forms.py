55eca15e2592d6019c013a3c782b0ac221a64685
ndmichael <ukejemicheal@gmail.com>
2022-06-20 00:49:31 +0100

    updated clients form for signup

--- clients/forms.py
+++ clients/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 from allauth.account.forms import SignupForm, LoginForm
 # from django_countries.fields import CountryField
 from django.utils import timezone
-from . import models
+from lottery import models
 
 
 class DateInput(forms.DateInput):
@@ -35,17 +35,31 @@ class MyCustomSignupForm(SignupForm):
     )
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last name')
-    phoneNumber = forms.CharField(required=False)
+    phone_number = forms.CharField(required=False)
     address = forms.CharField(widget=forms.Textarea(attrs={'name':'body', 'rows':'2'}))
     # country = CountryField(blank=True).formfield()
     dob = forms.DateField(widget=DateInput)
     city = forms.CharField(max_length=20, required=False)
     gender = forms.ChoiceField(choices=gender,  required=False)
 
-    
+    def save(self, request):
+            user = super(MyCustomSignupForm, self).save(request)
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.save()
+
+            models.Profile.objects.create(
+                user=user, 
+                phone_number=self.cleaned_data['phone_number'], 
+                city=self.cleaned_data['city'], 
+                address = self.cleaned_data['address'],
+                gender=self.cleaned_data['gender'], 
+                dob=self.cleaned_data['dob'],
+            )
+            return user
+
 
     def __init__(self, *args, **kwargs):
-
         super(MyCustomSignupForm, self).__init__(*args, **kwargs)
         self.fields["first_name"].widget.attrs["placeholder"] = ""
         self.fields["last_name"].widget.attrs["placeholder"] = ""
@@ -56,8 +70,9 @@ class MyCustomSignupForm(SignupForm):
 
         self.fields["first_name"].widget.attrs.update({'class': 'form-control-lg'})
         self.fields["last_name"].widget.attrs.update({'class': 'form-control-lg'})
+        self.fields["username"].widget.attrs.update({'class': 'form-control-lg'})
         self.fields["email"].widget.attrs.update({'class': 'form-control-lg'})
         self.fields["dob"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["phoneNumber"].widget.attrs.update({'class': 'form-control-lg'})
+        self.fields["phone_number"].widget.attrs.update({'class': 'form-control-lg'})
         self.fields["gender"].widget.attrs.update({'class': 'form-control-lg'})
         self.fields["city"].widget.attrs.update({'class': 'form-control-lg'})
