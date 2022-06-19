8e186b3a129f43995f0afab6bd43081553eec91f
ndmichael <ukejemicheal@gmail.com>
2022-06-20 00:48:53 +0100

    Added models to admin

--- lottery/admin.py
+++ lottery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Drawing, Ticket, Pick, WinningPick
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Drawing)
+admin.site.register(Ticket)
+admin.site.register(Pick)
