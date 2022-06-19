499a43cb429ad2aa56c5b396cfc64509b68af584
ndmichael <ukejemicheal@gmail.com>
2022-06-19 19:03:40 +0100

    updated urls for about and contact page

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, games
+from .views import index, games, draw, contact, about
 
 
 urlpatterns = [
     path('', index, name="index"),
-    path('games/', games, name="all-games")
+    path('games/', games, name="all-games"),
+    path('games/draws/', draw, name="pick"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about"),
 ]
