29f234dfa51906ad1a0874e41efb0e01b2dcaeb2
ndmichael <ukejemicheal@gmail.com>
2022-06-20 01:39:03 +0100

    added allowed host

--- swm/settings.py
+++ swm/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zg124=p1iuswqh2y@bcu)#@1c=ymock!=!7jy*rdbvv$r^+(uw'
+# SECRET_KEY = 'django-insecure-zg124=p1iuswqh2y@bcu)#@1c=ymock!=!7jy*rdbvv$r^+(uw'
+SECRET_KEY = os.environ.get("SECRET_KEY_BOIW")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 
-ALLOWED_HOSTS = []
+SITE_ID = 1
+
+ALLOWED_HOSTS = ['swmlottery.herokuapp.com', '']
 
 
 # Application definition
