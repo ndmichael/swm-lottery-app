404b1ea63cb8525ee440256e765b86486ef0cef6
ndmichael <ukejemicheal@gmail.com>
2022-06-20 10:10:07 +0100

    updated the settings.py

--- swm/settings.py
+++ swm/settings.py
@@ -162,6 +162,8 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "/accounts/login/"
 ACCOUNT_FORMS = {"signup": "clients.forms.MyCustomSignupForm", "login": "clients.forms.SelfLoginForm"}
 
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
