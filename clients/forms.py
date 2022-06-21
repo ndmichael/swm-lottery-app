d0b2e94983f568d42f608ab1e31ec9a7a856e670
ndmichael <ukejemicheal@gmail.com>
2022-06-21 03:15:24 +0100

    uodated clients page

--- clients/forms.py
+++ clients/forms.py
@@ -3,61 +3,64 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from allauth.account.forms import SignupForm, LoginForm
+
 # from django_countries.fields import CountryField
 from django.utils import timezone
 from lottery import models
 
 
 class DateInput(forms.DateInput):
-    input_type = 'date'
+    input_type = "date"
 
 
-class SelfLoginForm (LoginForm):
-    
+class SelfLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["login"].widget.attrs["placeholder"] = ""
         self.fields["password"].widget.attrs["placeholder"] = ""
-        
+
         self.fields["login"].widget.attrs.update(
-            {'class': 'form-control-lg bg-success border-0  border-bottom mb-3'})
+            {
+                "class": "form-control-lg bg-success text-white border-0  border-bottom mb-3"
+            }
+        )
         self.fields["password"].widget.attrs.update(
-            {'class': 'form-control-lg  bg-success border-0  border-bottom mb-3'})
+            {
+                "class": "form-control-lg  bg-success text-white border-0  border-bottom mb-3"
+            }
+        )
 
 
 class MyCustomSignupForm(SignupForm):
     # field_order = ['first_name', 'last_name',  'username',
     #                'email', 'password1', 'password2', 'balance','country', 'address', 'dob','image']
-    gender= (
-        ('male', 'MALE'),
-        ('female', 'FEMALE'),
-        ('others', 'OTHERS')
-    )
-    first_name = forms.CharField(max_length=30, label='First Name')
-    last_name = forms.CharField(max_length=30, label='Last name')
+    gender = (("male", "MALE"), ("female", "FEMALE"), ("others", "OTHERS"))
+    first_name = forms.CharField(max_length=30, label="First Name")
+    last_name = forms.CharField(max_length=30, label="Last name")
     phone_number = forms.CharField(required=False)
-    address = forms.CharField(widget=forms.Textarea(attrs={'name':'body', 'rows':'2'}))
+    address = forms.CharField(
+        widget=forms.Textarea(attrs={"name": "body", "rows": "2"})
+    )
     # country = CountryField(blank=True).formfield()
     dob = forms.DateField(widget=DateInput)
     city = forms.CharField(max_length=20, required=False)
-    gender = forms.ChoiceField(choices=gender,  required=False)
+    gender = forms.ChoiceField(choices=gender, required=False)
 
     def save(self, request):
-            user = super(MyCustomSignupForm, self).save(request)
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            user.save()
+        user = super(MyCustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.save()
 
-            models.Profile.objects.create(
-                user=user, 
-                phone_number=self.cleaned_data['phone_number'], 
-                city=self.cleaned_data['city'], 
-                address = self.cleaned_data['address'],
-                gender=self.cleaned_data['gender'], 
-                dob=self.cleaned_data['dob'],
-            )
-            return user
-
+        models.Profile.objects.create(
+            user=user,
+            phone_number=self.cleaned_data["phone_number"],
+            city=self.cleaned_data["city"],
+            address=self.cleaned_data["address"],
+            gender=self.cleaned_data["gender"],
+            dob=self.cleaned_data["dob"],
+        )
+        return user
 
     def __init__(self, *args, **kwargs):
         super(MyCustomSignupForm, self).__init__(*args, **kwargs)
@@ -68,11 +71,11 @@ class MyCustomSignupForm(SignupForm):
         self.fields["password1"].widget.attrs["placeholder"] = ""
         self.fields["password2"].widget.attrs["placeholder"] = ""
 
-        self.fields["first_name"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["last_name"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["username"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["email"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["dob"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["phone_number"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["gender"].widget.attrs.update({'class': 'form-control-lg'})
-        self.fields["city"].widget.attrs.update({'class': 'form-control-lg'})
+        self.fields["first_name"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["last_name"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["username"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["email"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["dob"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["phone_number"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["gender"].widget.attrs.update({"class": "form-control-lg"})
+        self.fields["city"].widget.attrs.update({"class": "form-control-lg"})
