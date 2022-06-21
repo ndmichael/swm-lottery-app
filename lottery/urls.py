d8c2e641d317a3a29455eef6c107c3ed9b138c46
ndmichael <ukejemicheal@gmail.com>
2022-06-21 03:12:48 +0100

    updated urls py for draw page

--- lottery/urls.py
+++ lottery/urls.py
@@ -3,9 +3,9 @@ from .views import index, games, draw, contact, about
 
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('games/', games, name="all-games"),
-    path('games/draws/', draw, name="pick"),
-    path('contact/', contact, name="contact"),
-    path('about/', about, name="about"),
+    path("", index, name="index"),
+    path("games/", games, name="all-games"),
+    path("games/draws/<str:type><int:id>/", draw, name="pick"),
+    path("contact/", contact, name="contact"),
+    path("about/", about, name="about"),
 ]
