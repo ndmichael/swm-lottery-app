295263723c663c9bb6112abb89182134fed0b4cc
ndmichael <ukejemicheal@gmail.com>
2022-06-26 10:40:07 +0100

    fixing and updating profile page

--- clients/views.py
+++ clients/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from lottery.models import Profile
+from lottery.models import Ticket
 
 # Create your views here.
 
 
 def profile(request, username):
     user = get_object_or_404(User, username=username)
-    context = {
-        "user": user,
-    }
+    tickets = Ticket.objects.filter(user_id=user.id).order_by("-date")
+    context = {"user": user, "tickets": tickets}
     return render(request, "account/profile.html", context)
