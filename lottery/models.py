0aa615d9d3fd40bbd8c97c2f6dae4c08c7291f89
ndmichael <ukejemicheal@gmail.com>
2022-06-27 08:53:41 +0100

    updated the enddate returns

--- lottery/models.py
+++ lottery/models.py
@@ -74,13 +74,13 @@ class Drawing(models.Model):
     @property
     def enddate(self):
         if self.type == "bronze":
-            return self.created + datetime.timedelta(hours=0, minutes=2)
+            return self.created + datetime.timedelta(hours=1, minutes=0)
         elif self.type == "silver":
-            return self.created + datetime.timedelta(hours=0, minutes=4)
+            return self.created + datetime.timedelta(hours=3, minutes=0)
         elif self.type == "gold":
-            return self.created + datetime.timedelta(hours=0, minutes=6)
+            return self.created + datetime.timedelta(hours=72, minutes=0)
         elif self.type == "platinum":
-            return self.created + datetime.timedelta(hours=0, minutes=8)
+            return self.created + datetime.timedelta(hours=168, minutes=0)
         else:
             return self.created
 
