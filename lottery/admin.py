7754205b9ec7dcaca254ec6ba59b8dc5a4d13353
ndmichael <ukejemicheal@gmail.com>
2022-07-12 08:41:01 +0100

    linked new models to admin

--- lottery/admin.py
+++ lottery/admin.py
@@ -9,6 +9,8 @@ from .models import (
     Silver,
     Gold,
     Platinum,
+    Jackpot,
+    Mega,
 )
 
 # Register your models here.
@@ -19,6 +21,8 @@ admin.site.register(Bronze)
 admin.site.register(Silver)
 admin.site.register(Gold)
 admin.site.register(Platinum)
+admin.site.register(Jackpot)
+admin.site.register(Mega)
 
 admin.site.register(Ticket)
 admin.site.register(Pick)
