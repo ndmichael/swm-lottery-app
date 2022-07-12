ba0d4cd50f328915ec397c29df37469dad0b1604
ndmichael <ukejemicheal@gmail.com>
2022-07-12 10:03:14 +0100

    updated context processor

--- lottery/context_processors.py
+++ lottery/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Bronze, Silver, Gold, Platinum
+from .models import Bronze, Silver, Gold, Platinum, Jackpot, Megawin
 
 
 def timer(request):
@@ -6,11 +6,15 @@ def timer(request):
     silver_data = Silver.objects.filter(status=True).first()
     gold_data = Gold.objects.filter(status=True).first()
     platinum_data = Platinum.objects.filter(status=True).first()
+    jackpot_data = Jackpot.objects.filter(status=True).first()
+    mega_data = Megawin.objects.filter(status=True).first()
 
     context = {
         "b_data": bronze_data,
         "s_data": silver_data,
         "g_data": gold_data,
         "p_data": platinum_data,
+        "j_data": jackpot_data,
+        "mega_data": mega_data,
     }
     return context
